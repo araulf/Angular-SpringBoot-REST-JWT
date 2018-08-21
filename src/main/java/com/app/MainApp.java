4e400980d9e4be69dfd8068ef0d283c1694f2d65
onkar singh <onkarsinghmnit@gmail.com>
2018-08-21 13:22:39 +0530

    Changes to make war run on weblogic

--- src/main/java/com/app/MainApp.java
+++ src/main/java/com/app/MainApp.java
@@ -2,32 +2,46 @@ package com.app;
 
 import javax.annotation.Resource;
 
+import org.springframework.boot.Banner;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.WebApplicationInitializer;
+
 import com.app.services.StorageService;
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.transaction.annotation.*;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages ={ "com.app.repo"})
 @EntityScan(basePackages ={ "com.app.model"})
 @EnableTransactionManagement
-public class MainApp implements CommandLineRunner {
+public class MainApp extends SpringBootServletInitializer implements CommandLineRunner, WebApplicationInitializer {
 	@Resource
 	StorageService storageService;
-
-	public static void main(String[] args) throws Exception {
-		new SpringApplication(MainApp.class).run(args);
-	}
 
 	@Override
 	public void run(String... arg) throws Exception {
 		storageService.deleteAll();
 		storageService.init();
 	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return configureApplication(builder);
+	}
+
+	public static void main(String[] args) {
+		configureApplication(new SpringApplicationBuilder()).run(args);
+	}
+
+	private static SpringApplicationBuilder configureApplication(SpringApplicationBuilder builder) {
+		return builder.sources(MainApp.class).bannerMode(Banner.Mode.OFF);
+	}
+	
 }
 
 
